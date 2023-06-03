5bf4a00ec2697adf096f965145d4560dd0622254
SebChw <sebastian.chwilczynski@gmail.com>
2023-06-03 12:10:14 +0200

    add: ml baseline

--- README.md
+++ README.md
@@ -34,10 +34,37 @@ So eventually you work with something like this. `I splitted the text as it is n
 
 ![](img/new_format.png)
 
+If you prefer integers over string as `ner_tags` run
+```py
+from utils import cast_ner_labels_to_int
+casted = cast_ner_labels_to_int(dataset_ner['train'])
+```
+
+After this you can easily take int2str and str2int functions from the ClassLabel feature that was created
+
+```py
+casted.features['ner_tags'].feature.int2str(1)
+casted.features['ner_tags'].feature.str2int(1)
+```
+
 
 **For evaluation we use HF evaluate library** Any model or pipeline should follow it's API. [Check this](https://huggingface.co/docs/evaluate/v0.4.0/en/custom_evaluator) to implement custom Pipeline. Check `baselines.py` and `test_stuff.ipynb` for an example of Simple Baseline with API good enough for HF evaluator.
 
-
-
-
 `Suggestion: Maybe we can try to use Spacy for this?`
+
+
+### Preparing fasttext embeddings
+Installing fasttext
+
+```shell
+git clone https://github.com/facebookresearch/fastText.git
+pip install fastText
+```
+
+Run model creation
+
+```py
+from utils import create_fasttext_model
+
+create_fasttext_model(dataset_ner['train'], "legal_eval.bin")
+```
--- utils.py
+++ utils.py
@@ -1,58 +1,86 @@
+from pathlib import Path
 from typing import List
 
-from datasets import DatasetDict, load_dataset
+from datasets import ClassLabel, DatasetDict, Features, Sequence, Value, load_dataset
+from tokenizers.pre_tokenizers import WhitespaceSplit
 
 from constants import DATA_PATH, DEV_JUDG, DEV_PREA, TRAIN_JUDG, TRAIN_PREA
 
 
-def get_hf_dataset(columns_to_remove = ["id", "meta"]):
+def get_hf_dataset(columns_to_remove=["id", "meta"]):
     """Returns a HuggingFace DatasetDict object with train and test splits
-    
+
     Args:
-        columns_to_remove (list, optional): List of columns to remove from the dataset. Defaults to ["id", "meta"]. 
+        columns_to_remove (list, optional): List of columns to remove from the dataset. Defaults to ["id", "meta"].
         If you don't want to remove anything pass an empty list.
     """
 
     if not DATA_PATH.exists():
         raise FileNotFoundError("Please run get_data.py first to download the data")
-    
-    train_dataset = load_dataset("json", data_files=[str(TRAIN_JUDG), str(TRAIN_PREA)])['train']
-    test_dataset = load_dataset("json", data_files=[str(DEV_JUDG), str(DEV_PREA)])['train']
-   
-    dataset = DatasetDict({'train': train_dataset, 'test': test_dataset}).remove_columns(columns_to_remove)
+
+    train_dataset = load_dataset("json", data_files=[str(TRAIN_JUDG), str(TRAIN_PREA)])[
+        "train"
+    ]
+    test_dataset = load_dataset("json", data_files=[str(DEV_JUDG), str(DEV_PREA)])[
+        "train"
+    ]
+
+    dataset = DatasetDict(
+        {"train": train_dataset, "test": test_dataset}
+    ).remove_columns(columns_to_remove)
     # We unnest the data
-    return dataset.map(lambda example: {"annotations": example["annotations"][0]['result'], "text": example['data']["text"]}, remove_columns=["data"])
+    # ! If we do strip here labels will be off!
+    return dataset.map(
+        lambda example: {
+            "annotations": example["annotations"][0]["result"],
+            "text": example["data"]["text"],
+        },
+        remove_columns=["data"],
+    )
 
 
 def parse_to_ner(dataset):
     """Parses the dataset to a more user friendly NER format
-    
+
     Args:
         dataset (DatasetDict): HuggingFace DatasetDict object with train and test splits
-    
+
     Returns:
         dataset (DatasetDict): parsed dataset
     """
+
+    # ! Withouth removing punctuations we end up with tokens like "," etc.
+    # ! Withouth stripping we end up with tokens like "" which leads to 0 embeddings!
     def get_labels(example):
         """Reformats annotations to a list of labels"""
-        text, annotations = example['text'], example['annotations']
+        tokenizer = WhitespaceSplit()
 
-        words = text.split(" ")
+        text, annotations = example["text"], example["annotations"]
+
+        # we must do it like this so that we have good allignment with original labels
+        words_offsets = tokenizer.pre_tokenize_str(text)
+        words = [word for word, offset in words_offsets]
+        offsets = [offset for word, offset in words_offsets]
+
         labels = ["O"] * len(words)
         if not annotations:
-            return {"tokens": text.split(" "), "ner_tags": labels}
+            return {"tokens": words, "ner_tags": labels}
 
         # We iterate over the annotations and we get the label, start and end position
         to_iterate = []
         for annotation in annotations:
-            annotation = annotation['value']
-            label, start, end = annotation['labels'][0], annotation['start'], annotation['end']
+            annotation = annotation["value"]
+            label, start, end = (
+                annotation["labels"][0],
+                annotation["start"],
+                annotation["end"],
+            )
             to_iterate.append((label, start, end))
 
         # We iterate over the words and we assign the label to words withing start and end position
         i_label = 0  # On which label we are currently at
-        current_char = 0  # On which character we are currently at
-        for n_word, word in enumerate(words):
+        for n_word, offset in enumerate(offsets):
+            current_char = offset[0]
             label, start, end = to_iterate[i_label]
             if current_char >= start and current_char <= end:
                 if current_char == start:
@@ -62,13 +90,11 @@ def parse_to_ner(dataset):
             if current_char > end:
                 i_label += 1  # we move to another label
 
-            current_char += len(word) + 1  # we add 1 for the space
-
             if i_label == len(to_iterate):
                 break
 
-        return {"tokens": text.split(" "), "ner_tags": labels}
-    
+        return {"tokens": words, "ner_tags": labels}
+
     return dataset.map(get_labels, remove_columns=["text", "annotations"])
 
 
@@ -93,3 +119,51 @@ def words_to_offsets(words: List[str], join_by: str):
         start = end + len(join_by) + 1
 
     return offsets
+
+
+def create_fasttext_model(dataset, name="model.bin"):
+    """Creates a fasttext model from a HF dataset"""
+    EMB_RESULTS = Path("embeddings")
+    EMB_RESULTS.mkdir(exist_ok=True)
+    corpora = " ".join(
+        [" ".join(tokens) for tokens in dataset["tokens"]]
+    )  # I don't even preprocess the data this is to be discussed
+    corpora_path = EMB_RESULTS / "corpora.txt"
+    with open(corpora_path, "w") as f:
+        f.write(corpora)
+
+    import fasttext  # So that we don't have to install it if we don't need it
+
+    model = fasttext.train_unsupervised(str(corpora_path))
+    model.save_model(str(EMB_RESULTS / name))
+
+
+def get_unique_ner(dataset):
+    unique_labels = set()
+
+    for tags in dataset["ner_tags"]:
+        unique_labels = unique_labels.union(set(tags))
+
+    return list(unique_labels)
+
+
+def cast_ner_labels_to_int(dataset):
+    unique_ner = get_unique_ner(dataset)
+
+    casted = dataset.cast(
+        Features(
+            {
+                "ner_tags": Sequence(ClassLabel(names=unique_ner)),
+                "tokens": Sequence(Value(dtype="string")),
+            }
+        )
+    )
+
+    return casted
+
+
+def create_embeddings(example, emb_model):
+    example["embedding"] = [
+        emb_model.get_word_vector(word) for word in example["tokens"]
+    ]
+    return example
