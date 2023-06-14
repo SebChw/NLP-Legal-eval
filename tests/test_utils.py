606f7e5c9b73c2b656e62f2f19e8f67b05f42881
Kacper Trebacz <trebacz.kacper@gmail.com>
2023-06-14 13:12:58 +0200

    test_utils flake update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,5 +7,5 @@ def test_get_class_counts(casted_dataset_dict):
     class_counts = get_class_counts(casted_dataset_dict["train"])
     # fmt: off
     np.array_equal(class_counts, np.array([1., 3., 2., 3., 81., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.,
-                                            0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]))
+                                           0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]))
     # fmt: on
